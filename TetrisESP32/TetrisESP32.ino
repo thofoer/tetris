59c566a99d28c0f3e3d21376d7cfdeae1779c2a2
qxv2786 <qxv2786@bmwgroup.net>
2021-03-12 15:06:33 +0100

    Weiterentwicklung. Header-Datei angelegt.

--- TetrisESP32/TetrisESP32.ino
+++ TetrisESP32/TetrisESP32.ino
@@ -1,23 +1,7 @@
 #include <BluetoothSerial.h>
 #include <FastLED.h>
+#include "definitions.h"
 
-#define CMD_RIGHT 'r'
-#define CMD_LEFT 'l'
-#define CMD_TURN 't'
-#define CMD_DOWN 'd'
-#define CMD_START 's'
-#define CMD_RESET 'x'
-
-#define MSG_LEVEL 'L'
-#define MSG_SCORE 'S'
-
-#define tileO 0
-#define tileI 1
-#define tileT 2
-#define tileZ 3
-#define tileS 4
-#define tileL 5
-#define tileJ 6
 
 #define WIDTH 8
 #define HEIGHT 8
@@ -27,6 +11,8 @@
 #define BRIGHTNESS  32
 #define LED_TYPE    WS2811
 #define COLOR_ORDER GRB
+#define FRAME_TIME_MS 10
+
 
 
 CRGB leds[NUM_LEDS];
@@ -34,232 +20,122 @@ CRGB leds[NUM_LEDS];
 BluetoothSerial SerialBT;
 
 int level = 1;
-int tileId = 0;
+int tileId = 4;
 int posX = 2;
 int posY = 0;
 int rot = 0;
 
+int speed = 1;
+int fallCounter = speed;
+
 int matrix[WIDTH][HEIGHT];
 
-const int tiles[4][7][4][4] ={ 
-  
-                       // 0°
-                      { 
-                      { {0, 0, 0, 0},    // O 
-                        {0, 1, 1, 0},
-                        {0, 1, 1, 0},
-                        {0, 0, 0, 0} },
-
-                      { {0, 0, 0, 0},    // I
-                        {1, 1, 1, 1},
-                        {0, 0, 0, 0},
-                        {0, 0, 0, 0} },
-
-                      { {0, 0, 0, 0},    // T
-                        {1, 1, 1, 0},
-                        {0, 1, 0, 0},
-                        {0, 0, 0, 0} },
-            
-                      { {0, 0, 0, 0},    // Z
-                        {1, 1, 0, 0},
-                        {0, 1, 1, 0},
-                        {0, 0, 0, 0} },
-
-                      { {0, 0, 0, 0},    // S
-                        {0, 1, 1, 0},
-                        {1, 1, 0, 0},
-                        {0, 0, 0, 0} },
-
-                      { {0, 1, 0, 0},    // L
-                        {0, 1, 0, 0},
-                        {0, 1, 1, 0},
-                        {0, 0, 0, 0} },
-
-                      { {0, 0, 1, 0},    // J
-                        {0, 0, 1, 0},
-                        {0, 1, 1, 0},
-                        {0, 0, 0, 0} }
-                     },
-                     
-                     // 90°
-                     {   
-                      { {0, 0, 0, 0},    // O 
-                        {0, 1, 1, 0},
-                        {0, 1, 1, 0},
-                        {0, 0, 0, 0} },
-
-                      { {0, 1, 0, 0},    // I
-                        {0, 1, 0, 0},
-                        {0, 1, 0, 0},
-                        {0, 1, 0, 0} },
-
-                      { {0, 1, 0, 0},    // T
-                        {1, 1, 0, 0},
-                        {0, 1, 0, 0},
-                        {0, 0, 0, 0} },
-            
-                      { {0, 0, 1, 0},    // Z
-                        {0, 1, 1, 0},
-                        {0, 1, 0, 0},
-                        {0, 0, 0, 0} },
-
-                      { {0, 1, 0, 0},    // S
-                        {0, 1, 1, 0},
-                        {0, 0, 1, 0},
-                        {0, 0, 0, 0} },
-
-                      { {0, 0, 0, 0},    // L
-                        {1, 1, 1, 0},
-                        {1, 0, 0, 0},
-                        {0, 0, 0, 0} },
-
-                      { {0, 0, 0, 0},    // J
-                        {1, 0, 0, 0},
-                        {1, 1, 1, 0},
-                        {0, 0, 0, 0} }
-                     },
-                     // 180°
-                     {
-                      { {0, 0, 0, 0},    // O 
-                        {0, 1, 1, 0},
-                        {0, 1, 1, 0},
-                        {0, 0, 0, 0} },
-
-                      { {0, 0, 0, 0},    // I
-                        {1, 1, 1, 1},
-                        {0, 0, 0, 0},
-                        {0, 0, 0, 0} },
-
-                      { {0, 1, 0, 0},    // T
-                        {1, 1, 1, 0},
-                        {0, 0, 0, 0},
-                        {0, 0, 0, 0} },
-            
-                      { {0, 0, 0, 0},    // Z
-                        {1, 1, 0, 0},
-                        {0, 1, 1, 0},
-                        {0, 0, 0, 0} },
-
-                      { {0, 0, 0, 0},    // S
-                        {0, 1, 1, 0},
-                        {1, 1, 0, 0},
-                        {0, 0, 0, 0} },
-
-                      { {0, 1, 1, 0},    // L
-                        {0, 0, 1, 0},
-                        {0, 0, 1, 0},
-                        {0, 0, 0, 0} },
-
-                      { {0, 1, 1, 0},    // J
-                        {0, 1, 0, 0},
-                        {0, 1, 0, 0},
-                        {0, 0, 0, 0} },
-                     },
-                   // 270°
-                     { 
-                      { {0, 0, 0, 0},    // O 
-                        {0, 1, 1, 0},
-                        {0, 1, 1, 0},
-                        {0, 0, 0, 0} },
-
-                      { {0, 1, 0, 0},    // I
-                        {0, 1, 0, 0},
-                        {0, 1, 0, 0},
-                        {0, 1, 0, 0} },
-
-                      { {0, 1, 0, 0},    // T
-                        {0, 1, 1, 0},
-                        {0, 1, 0, 0},
-                        {0, 0, 0, 0} },
-            
-                      { {0, 0, 1, 0},    // Z
-                        {0, 1, 1, 0},
-                        {0, 1, 0, 0},
-                        {0, 0, 0, 0} },
-
-                      { {0, 1, 0, 0},    // S
-                        {0, 1, 1, 0},
-                        {0, 0, 1, 0},
-                        {0, 0, 0, 0} },
-
-                      { {0, 0, 0, 0},    // L
-                        {0, 0, 1, 0},
-                        {1, 1, 1, 0},
-                        {0, 0, 0, 0} },
-
-                      { {0, 0, 0, 0},    // J
-                        {1, 1, 1, 0},
-                        {0, 0, 1, 0},
-                        {0, 0, 0, 0} }
-
-                     }};
-
-// Anzahl freie Spalten von Links für jedes Teil für alle 4 Rotationen
-
-const int offsetLeft[4][7] = { {1, 0, 0, 0, 0, 1, 1 },
-                               {1, 1, 0, 1, 1, 0, 0 },
-                               {1, 0, 0, 0, 0, 1, 1 },
-                               {1, 1, 1, 1, 1, 0, 0 } };
-
-                           
- 
-const CRGB colors[7] = { CRGB(127, 127, 0), CRGB::MidnightBlue, CRGB::Green, CRGB::Indigo, CRGB::MediumTurquoise, CRGB::Crimson, CRGB::DarkOrange };
-
 void setup() {
-  delay( 3000 ); // power-up safety delay
+  delay( 1000 ); // power-up safety delay
   FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
   FastLED.setBrightness(  BRIGHTNESS );
     
   Serial.begin(115200);
-  SerialBT.begin("Tetris"); //Name des ESP32
-  Serial.println("Der ESP32 ist bereit. Verbinde dich nun über Bluetooth.");
-
+  SerialBT.begin("Tetris"); // Bluetooth-Name des ESP32
+  Serial.println("Der ESP32 ist bereit. Verbinde dich nun über Bluetooth.");  
+  
   for (int x=0; x<WIDTH; x++) {
     for (int y=0; y<HEIGHT; y++) {
-      matrix[x][y] = 0;
+      matrix[x][y] = EMPTY_PIX;
       leds[x*WIDTH+y] = CRGB::Black;
     }
+  }  
+
+// Frame in matrix zeichnen
+  for (int i=0; i<HEIGHT; i++) {
+    matrix[0][i] = FRAME_PIX;
+    matrix[WIDTH-1][i] = FRAME_PIX;
+  }
+  for (int i=1; i<WIDTH-1; i++) {
+    matrix[i][HEIGHT-1] = FRAME_PIX;
   }
 }
 
-void clearLeds() {
-  for (int x=0; x<WIDTH; x++) {
-    for (int y=0; y<HEIGHT; y++) {   
-      leds[x*WIDTH+y] = CRGB::Black;
+void loop() {  
+  receiveCommand();
+  clearLeds();
+  drawMatrix();
+  //moveDown();
+  showTile();
+  FastLED.show();  
+  delay(FRAME_TIME_MS);
+}
+
+void moveDown() {
+  fallCounter--;
+  if (fallCounter<=0) {
+    if (!isCollision(0, 0, 1)) {
+      posY++;
+    }
+    else {
+      // unten angekommen
+      posY = 0;
+    }
+    fallCounter = speed;
+  }
+}
+
+void dumpTile() {
+  for (int y=0; y<4; y++) {
+    for (int x=0; x<4; x++) {
+      Serial.printf("%d", tiles[rot][tileId][y][x]);
+    }
+    Serial.printf("\n");
+  }
+}
+
+void dumpMatrix(int sx, int sy) {
+  for (int y=0; y<4; y++) {
+    for (int x=0; x<4; x++) {
+      Serial.printf("%d", matrix[sx+x][sy+y]);
+    }
+    Serial.printf("\n");
+  }
+}
+
+boolean isCollision(int dRot, int dX, int dY) {
+  Serial.printf("-----------------\nrot=%d, coord=%d/%d\n", rot, posX, posY);
+  dumpTile();
+  Serial.printf("--\n");
+  dumpMatrix(posX+dX, posY+dY);
+  boolean collision = false;
+  for (int x=0; x<4 && !collision; x++) {
+    for (int y=0; y<4 && !collision; y++) {
+       int tilePixel = tiles[(rot+dRot)%4][tileId][y][x];
+       if (tilePixel) {
+         int backgroundPixel = matrix[posX+x+dX][posY+y+dY];
+         if (backgroundPixel) {
+           collision=true;
+           Serial.printf("Kollision: %d/%d - %d\n", x, y, backgroundPixel);
+         }
+       }
     }
   }
+  return collision;
 }
 
-void showTile() {
-  setLed(0,0, CRGB::DarkGrey);
-  setLed(7,0, CRGB::DarkBlue);
-  for (int x=0; x<4; x++) {
-    for (int y=0; y<4; y++) {
-      int pixel = tiles[rot][tileId][y][x];
-      if (pixel) {
-        setLed(x+posX, y+posY, colors[tileId]);
-      }
-    }
-  }
-}
 
-void loop() {
-  
+void receiveCommand() {
   char input;
   
   if (SerialBT.available()) {
     input = (char) SerialBT.read();
     switch (input) {
       case CMD_RIGHT: right(); break;
-      case CMD_LEFT: left(); break;
-      case CMD_TURN: turn(); break;
+      case CMD_LEFT:  left();  break;
+      case CMD_TURN:  turn();  break;
+      case CMD_DOWN:  down();  break;
+      case CMD_START: start(); break;
+      case CMD_RESET: reset(); break;
     }
   }
-  delay(100);
-  clearLeds();
-  showTile();
-  FastLED.show();
 }
+
 
 void right() {
    Serial.println("rechts");
@@ -268,6 +144,7 @@ void right() {
    b[1] = level;
    SerialBT.write(b, 2);   
    level++;
+   moveDown();
 }
 
 void left() {
@@ -276,15 +153,56 @@ void left() {
    b[0] = MSG_SCORE;
    b[1] = 3;
    SerialBT.write(b, 2);   
-   tileId = (tileId+1) %7;
+   tileId = (tileId + 1) % 7;
 }
 
 void turn() {
-  rot = (rot +1 ) % 4;
+  if (!isCollision(1, 0, 0)) {
+    rot = (rot + 1) & 0x3;
+  }
+}
+
+void down() {
+  
+}
+void start() {
+  
+}
+void reset() {
+  
+}
+
+void showTile() {
+  for (int x=0; x<4; x++) {
+    for (int y=0; y<4; y++) {
+      int pixel = tiles[rot][tileId][y][x];
+      if (pixel) {
+        setLed(x+posX, y+posY, colors[tileId+1]);
+      }
+    }
+  }
+}
+
+void drawMatrix() {
+  for (int x=0; x<WIDTH; x++) {
+    for (int y=0; y<HEIGHT; y++) {
+      if (matrix[x][y]) {
+        setLed(x, y, colors[matrix[x][y]]);
+      }
+    }
+  }
 }
 
 void setLed(int x, int y, CRGB color) {
   int i = y*WIDTH;
   i += (y&1) ? x : (WIDTH-x-1);
   leds[i] = color;
+}
+
+void clearLeds() {
+  for (int x=0; x<WIDTH; x++) {
+    for (int y=0; y<HEIGHT; y++) {   
+      leds[x*WIDTH+y] = CRGB::Black;
+    }
+  }
 }
---
+++ TetrisESP32/definitions.h
@@ -0,0 +1,198 @@
+
+// von TetrisControl empfangene Kommandos
+#define CMD_RIGHT 'r'
+#define CMD_LEFT 'l'
+#define CMD_TURN 't'
+#define CMD_DOWN 'd'
+#define CMD_START 's'
+#define CMD_RESET 'x'
+
+// an TetrisControl gesendete Messages
+#define MSG_LEVEL 'L'
+#define MSG_SCORE 'S'
+
+// Pixel ID in matrix-Array
+#define  EMPTY_PIX  0
+#define  TILE_O_PIX  1
+#define  TILE_I_PIX  2
+#define  TILE_T_PIX  3
+#define  TILE_Z_PIX  4
+#define  TILE_S_PIX  5
+#define  TILE_L_PIX  6
+#define  TILE_J_PIX  7
+#define  FRAME_PIX   8
+
+// Indizes der Teile im tiles Array
+#define  TILE_O  0
+#define  TILE_I  1
+#define  TILE_T  2
+#define  TILE_Z  3
+#define  TILE_S  4
+#define  TILE_L  5
+#define  TILE_J  6
+
+// Farben für Teile und Rahmen
+#define COLOR_FRAME  CRGB(28, 11, 3)
+#define COLOR_TILE_O CRGB(127, 127, 0)
+#define COLOR_TILE_I CRGB::MidnightBlue
+#define COLOR_TILE_T CRGB::Green
+#define COLOR_TILE_Z CRGB::Indigo
+#define COLOR_TILE_S CRGB::MediumTurquoise
+#define COLOR_TILE_L CRGB::Crimson
+#define COLOR_TILE_J CRGB::DarkOrange
+
+
+const int tiles[4][7][4][4] ={ 
+  
+                       // 0°
+                      { 
+                      { {0, 0, 0, 0},    // O 
+                        {0, 1, 1, 0},
+                        {0, 1, 1, 0},
+                        {0, 0, 0, 0} },
+
+                      { {0, 0, 0, 0},    // I
+                        {1, 1, 1, 1},
+                        {0, 0, 0, 0},
+                        {0, 0, 0, 0} },
+
+                      { {0, 0, 0, 0},    // T
+                        {1, 1, 1, 0},
+                        {0, 1, 0, 0},
+                        {0, 0, 0, 0} },
+            
+                      { {0, 0, 0, 0},    // Z
+                        {1, 1, 0, 0},
+                        {0, 1, 1, 0},
+                        {0, 0, 0, 0} },
+
+                      { {0, 0, 0, 0},    // S
+                        {0, 1, 1, 0},
+                        {1, 1, 0, 0},
+                        {0, 0, 0, 0} },
+
+                      { {0, 1, 0, 0},    // L
+                        {0, 1, 0, 0},
+                        {0, 1, 1, 0},
+                        {0, 0, 0, 0} },
+
+                      { {0, 0, 1, 0},    // J
+                        {0, 0, 1, 0},
+                        {0, 1, 1, 0},
+                        {0, 0, 0, 0} }
+                     },
+                     
+                     // 90°
+                     {   
+                      { {0, 0, 0, 0},    // O 
+                        {0, 1, 1, 0},
+                        {0, 1, 1, 0},
+                        {0, 0, 0, 0} },
+
+                      { {0, 1, 0, 0},    // I
+                        {0, 1, 0, 0},
+                        {0, 1, 0, 0},
+                        {0, 1, 0, 0} },
+
+                      { {0, 1, 0, 0},    // T
+                        {1, 1, 0, 0},
+                        {0, 1, 0, 0},
+                        {0, 0, 0, 0} },
+            
+                      { {0, 0, 1, 0},    // Z
+                        {0, 1, 1, 0},
+                        {0, 1, 0, 0},
+                        {0, 0, 0, 0} },
+
+                      { {0, 1, 0, 0},    // S
+                        {0, 1, 1, 0},
+                        {0, 0, 1, 0},
+                        {0, 0, 0, 0} },
+
+                      { {0, 0, 0, 0},    // L
+                        {1, 1, 1, 0},
+                        {1, 0, 0, 0},
+                        {0, 0, 0, 0} },
+
+                      { {0, 0, 0, 0},    // J
+                        {1, 0, 0, 0},
+                        {1, 1, 1, 0},
+                        {0, 0, 0, 0} }
+                     },
+                     // 180°
+                     {
+                      { {0, 0, 0, 0},    // O 
+                        {0, 1, 1, 0},
+                        {0, 1, 1, 0},
+                        {0, 0, 0, 0} },
+
+                      { {0, 0, 0, 0},    // I
+                        {1, 1, 1, 1},
+                        {0, 0, 0, 0},
+                        {0, 0, 0, 0} },
+
+                      { {0, 1, 0, 0},    // T
+                        {1, 1, 1, 0},
+                        {0, 0, 0, 0},
+                        {0, 0, 0, 0} },
+            
+                      { {0, 0, 0, 0},    // Z
+                        {1, 1, 0, 0},
+                        {0, 1, 1, 0},
+                        {0, 0, 0, 0} },
+
+                      { {0, 0, 0, 0},    // S
+                        {0, 1, 1, 0},
+                        {1, 1, 0, 0},
+                        {0, 0, 0, 0} },
+
+                      { {0, 1, 1, 0},    // L
+                        {0, 0, 1, 0},
+                        {0, 0, 1, 0},
+                        {0, 0, 0, 0} },
+
+                      { {0, 1, 1, 0},    // J
+                        {0, 1, 0, 0},
+                        {0, 1, 0, 0},
+                        {0, 0, 0, 0} },
+                     },
+                   // 270°
+                     { 
+                      { {0, 0, 0, 0},    // O 
+                        {0, 1, 1, 0},
+                        {0, 1, 1, 0},
+                        {0, 0, 0, 0} },
+
+                      { {0, 1, 0, 0},    // I
+                        {0, 1, 0, 0},
+                        {0, 1, 0, 0},
+                        {0, 1, 0, 0} },
+
+                      { {0, 1, 0, 0},    // T
+                        {0, 1, 1, 0},
+                        {0, 1, 0, 0},
+                        {0, 0, 0, 0} },
+            
+                      { {0, 0, 1, 0},    // Z
+                        {0, 1, 1, 0},
+                        {0, 1, 0, 0},
+                        {0, 0, 0, 0} },
+
+                      { {0, 1, 0, 0},    // S
+                        {0, 1, 1, 0},
+                        {0, 0, 1, 0},
+                        {0, 0, 0, 0} },
+
+                      { {0, 0, 0, 0},    // L
+                        {0, 0, 1, 0},
+                        {1, 1, 1, 0},
+                        {0, 0, 0, 0} },
+
+                      { {0, 0, 0, 0},    // J
+                        {1, 1, 1, 0},
+                        {0, 0, 1, 0},
+                        {0, 0, 0, 0} }
+
+                     }};
+
+const CRGB colors[9] = {CRGB::Black, COLOR_TILE_O, COLOR_TILE_I, COLOR_TILE_T, COLOR_TILE_Z, COLOR_TILE_S, COLOR_TILE_L, COLOR_TILE_J, COLOR_FRAME };
